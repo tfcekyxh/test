4ae7b5edd67d8d1b9a2addb5bfc3a4dbaf3aa7ed
ljt <2073887899@qq.com>
2024-03-07 22:11:15 +0800

    ljt的第一次提交

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void LiuJianTaoFunction(){
+	printf("ljt is already\n");
+}
+int main(int argv,char *args[]){
+	char name[20];
+	scanf("%s",name);
+	if(!strcmp(name,"ljt")){
+		LiuJianTaoFunction();
+	}else{
+		printf("hello world\n");
+	}
+}
