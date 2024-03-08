ac4718716c69f1c04a5d2654ea278e992c22e62c
ljt <2073887899@qq.com>
2024-03-08 15:54:06 +0800

    ljt第一次提交

--- test.cpp
+++ test.cpp
@@ -2,22 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-void cow(){
-}
 void LiuJianTaoFunction(){
 	printf("ljt is already\n");
-}
-void cat(){
 }
 int main(int argv,char *args[]){
 	char name[20];
 	scanf("%s",name);
 	if(!strcmp(name,"ljt")){
 		LiuJianTaoFunction();
-	}else{
-		printf("hello world\n");
 	}
-	cat();
-	cow();
 }
 
